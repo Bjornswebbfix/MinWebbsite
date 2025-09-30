0f2b98d41175c67aa5a3b37768d00c568ce6ccff
Bjornswebbfix <bjorn.ahlfors0407@gmail.com>
2025-09-30 17:05:24 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-document.getElementById("learnMoreBtn").addEventListener("click", () => {
-  const section = document.getElementById("featured");
-  section.scrollIntoView({ behavior: "smooth" });
-});
+const canvas = document.getElementById('wave');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = 150;
+
+let t = 0;
+
+function drawWave() {
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  ctx.fillStyle = 'rgba(0,240,255,0.3)';
+  ctx.beginPath();
+  for(let x = 0; x < canvas.width; x++) {
+    let y = 20 * Math.sin((x+t)/50) + 40 * Math.sin((x+t)/100);
+    ctx.lineTo(x, y);
+  }
+  ctx.lineTo(canvas.width, canvas.height);
+  ctx.lineTo(0, canvas.height);
+  ctx.closePath();
+  ctx.fill();
+  t += 2;
+  requestAnimationFrame(drawWave);
+}
+
+drawWave();
