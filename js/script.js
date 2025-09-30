bdedc0cc2d365dc83862e155556e3fd4a47bf6be
Bjornswebbfix <bjorn.ahlfors0407@gmail.com>
2025-09-30 16:21:34 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("learnMoreBtn").addEventListener("click", () => {
+  const section = document.getElementById("featured");
+  section.scrollIntoView({ behavior: "smooth" });
+});
